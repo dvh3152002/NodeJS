a0fc0c01d183dc412c47cc6aefd0f7c4045097ff
dvh3152002 <dvh31502@gmail.com>
2022-10-13 23:35:18 +0700

    first commit

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -16,6 +16,15 @@ let getAboutPage=(req,res)=>{
     return res.render('test/aboutPage.ejs')
 }
 
+let getCRUD=(req,res)=>{
+    return res.render('crud.ejs');
+}
+
+let postCRUD=(req,res)=>{
+    console.log(req.body);
+    return res.send('post crud from server')
+}
+
 // object: {
 //     key: '',
 //     value: ''
@@ -24,4 +33,6 @@ let getAboutPage=(req,res)=>{
 module.exports={
     getHomePage:getHomePage,
     getAboutPage:getAboutPage,
+    getCRUD:getCRUD,
+    postCRUD:postCRUD,
 }
